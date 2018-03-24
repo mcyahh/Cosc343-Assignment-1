5f5e39e29cb8673103c7d52f862bb577e2582974
Alexander Galbraith <galal567@student.otago.ac.nz>
2018-03-24 15:03:27 +1300

    Mostly works, sonar is dodgy.

--- tile_tester.py
+++ tile_tester.py
@@ -10,6 +10,8 @@ btn = Button()
 cl = ColorSensor()
 # Connect EV3 color sensor.
 cl = ColorSensor()
+
+ts = TouchSensor()
 try:
     #Connect to sonar
     sn = UltrasonicSensor()
@@ -26,7 +28,6 @@ except:
 
 
 
-# reports 'cm' even though the sensor measures 'mm'
 
 
 pair = LargeMotorPair(OUTPUT_B, OUTPUT_C)
@@ -63,11 +64,6 @@ def moveForward(speed = 100, len = 360):
     #if not pair.is_running:
     pair.run_to_rel_pos(speed_sp = speed * speedMult, position_sp = len)
     Leds.set_color(Leds.LEFT, Leds.GREEN)
-
-
-def rotate(angle):
-    mC.run_to_rel_pos(speed_sp = 100 * speedMult, position_sp = angle * 3)
-    mB.run_to_rel_pos(speed_sp = 100 * speedMult, position_sp=-angle * 3)
 
 def sample():
     return cl.value()
@@ -145,33 +141,38 @@ def sweep_sonar_smooth(angle):
     while pair.is_running:
         maxcount += 1
         dist = sn.value()
-        if dist > min:
-            count = i
+        if dist < min:
             min = dist
             count_point = maxcount
+        print_stuff("CP, MC: " + str(count_point) + " , " + str(maxcount))
+        sleep(0.01)
 
     rotate(-angle)
     pair.wait_until_not_moving()
-    return ((count_point/maxcount) * angle, min)
+    print_stuff("CP, MC: "+ str(count_point) + " , " + str(maxcount))
+    return ((float(count_point)/float(maxcount)) * angle, min)
 
 def sonar_find_min():
-    (langle, lval) = sweep_sonar_smooth(-90)
-    (rangle, rval) = sweep_sonar_smooth(90)
+    (langle, lval) = sweep_sonar_smooth(-45)
+    (rangle, rval) = sweep_sonar_smooth(45)
     angle = langle if lval < rval else rangle
+
+    if (angle == 0):
+        return
     rotate(angle)
     pair.wait_until_not_moving()
+    return min(lval,rval)
 
-
-def rotate(angle, fwd = 0):
-    mC.run_to_rel_pos(speed_sp = 50 * speedMult, position_sp = angle * 3 + fwd)
-    mB.run_to_rel_pos(speed_sp = 50 * speedMult, position_sp=-angle * 3 + fwd)
+def rotate(angle, fwd = 0, speed = 50):
+    mC.run_to_rel_pos(speed_sp = speed * speedMult, position_sp = angle * 3 + fwd)
+    mB.run_to_rel_pos(speed_sp = speed * speedMult, position_sp=-angle * 3 + fwd)
 
 
 def move_to_next_black():
     print_stuff("Moving")
     global black_tile_count
     on_black = True
-    moveForward(50, 360)
+    moveForward(50, 720)
     while(pair.is_running):
         (av,std) = calc_current()
         if is_on_white(av) and on_black:
@@ -179,9 +180,9 @@ def move_to_next_black():
         if is_on_black(av) and not on_black:
             black_tile_count += 1
             pair.stop()
-            moveForward(50, 90)
+            moveForward(50, 45)
             pair.wait_until_not_moving()
-            Sound.beep()
+            Sound.speak(black_tile_count)
             return True
     return False
 
@@ -212,23 +213,66 @@ def sweep_check():
     #we are on a black tile, sweep for a white tile
     valL = sweep(-ROT_)
     valR = sweep(ROT_)
-    val = valL + ROT_ if abs(valL) < abs(valR) else valR - ROT_
-    rotate(val/3)
-
+    val = valL + valR
+    if not val == 0:
+        rotate(val/7)
+        print_stuff("Val: "+str(val))
+        pair.wait_until_not_moving()
 
 
 try:
-    while False:
+
+    move_to_next_black()
+    moveForward(50, 30)
+    pair.wait_until_not_moving()
+    rotate(-60)
+    pair.wait_until_not_moving()
+    moveForward((50,-30))
+    pair.wait_until_not_moving()
+    sweep_check()
+    while True:
         if move_to_next_black():
             sweep_check()
         if black_tile_count == 15:
             for i in range(5):
                 Sound.beep()
             break
-    while True:
-        sonar_find_min()
-        moveForward(100,90)
-        pair.wait_until_not_moving()
+    rotate(-60)
+    pair.wait_until_not_moving()
+    moveForward(200,4080)
+    pair.wait_until_not_moving()
+    seek_and_destroy = True
+    while seek_and_destroy:
+        val = sonar_find_min()
+        print_stuff("MIN: " + str(val))
+        Sound.beep()
+        #if we are super close
+        if val<40:
+            seek_and_destroy = False
+            Sound.speak("Prepare to die, bottle!")
+            sleep(2)
+        else:
+            moveForward(400,360)
+
+        while(pair.is_running):
+            #probably wont work
+            if ts.value() > 0.1:
+                pair.stop()
+                seek_and_destroy = False
+                Sound.speak("Prepare to die, bottle!")
+                sleep(0.5)
+                break
+
+    if not sample()>black_average + black_stdev:
+        move_to_next_black()
+    moveForward(500,720)
+    while pair.is_running:
+        if sample()>black_average + black_stdev:
+            pair.stop()
+            break
+    Sound.speak("Victory! Take that bottle!")
+    sleep(1)
+
 except:
     import traceback
     exc_type, exc_value, exc_traceback = sys.exc_info()
